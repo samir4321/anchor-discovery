26e30e9a7ef0b7bcd49f5ffd2b0f3714347ce479
samir <samir@traceable.ai>
2019-09-11 11:29:08 -0700

    minor refactor

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask_jwt_extended import (
     JWTManager, jwt_required, create_access_token,
     get_jwt_identity
 )
+import time
 
 app = Flask(__name__)
 
@@ -10,7 +11,9 @@ app = Flask(__name__)
 app.config['JWT_SECRET_KEY'] = 'super-secret'  # Change this!
 jwt = JWTManager(app)
 
-USERS = ['bob', 'alice', 'admin']
+
+NORMAL_USERS = ['bob', 'alice']
+USERS = NORMAL_USERS + ['admin']
 
 ADMIN_DATABASE = {
     'bob': 'came in last thursday',
@@ -21,6 +24,9 @@ USER_DATABASE = {
     'bob': 'bobs stuff',
     'alice': 'alices stuff'
 }
+
+GLOBAL_LOG = {}
+
 
 # Provide a method to create access tokens. The create_access_token()
 # function is used to actually generate the token, and you can return
@@ -36,17 +42,17 @@ def login():
     if username == 'admin':
         access_token = create_access_token(identity=username)
         return jsonify(access_token=access_token), 200
-
     if not username:
         return jsonify({"msg": "Missing username parameter"}), 400
     if not password:
         return jsonify({"msg": "Missing password parameter"}), 400
-
-    if username != 'test' or password != 'test':
+    if username not in USERS or password != 'test':
         return jsonify({"msg": "Bad username or password"}), 401
 
     # Identity can be any data that is json serializable
     access_token = create_access_token(identity=username)
+    current_milli_time = lambda: int(round(time.time() * 1000))
+    GLOBAL_LOG[access_token] = { "login_time": current_milli_time, "endpoints": [] }
     return jsonify(access_token=access_token), 200
 
 
@@ -60,10 +66,13 @@ def protected():
     return jsonify(logged_in_as=current_user), 200
 
 
+def get_access_token():
+    return request.headers.get("Authorization").split("Bearer")[1].strip()
+
+
 @app.route('/users/all', methods=["GET"])
 @jwt_required
 def get_all_users():
-    all_users = ['bob', 'alice']
     current_user = get_jwt_identity()
     if current_user != 'admin':
         return jsonify(err='unauthorized'), 403
@@ -73,7 +82,6 @@ def get_all_users():
 @app.route('/records', methods=['GET'])
 @jwt_required
 def get_admin_records():
-    all_users = ['bob', 'alice']
     current_user = get_jwt_identity()
     if current_user != 'admin':
         return jsonify(err='unauthorized'), 403
@@ -81,9 +89,9 @@ def get_admin_records():
     return jsonify(records=ADMIN_DATABASE), 200
 
 
-@app.route('/record/<id>', methods=['GET'])
+@app.route('/record', methods=['GET'])
 @jwt_required
-def user_record(id):
+def user_record():
     current_user = get_jwt_identity()
     user_record = USER_DATABASE.get(current_user)
     user_record_q(current_user)
@@ -91,11 +99,36 @@ def user_record(id):
 
 
 def user_record_q(user):
-    print(f'SELECT * FROM USER_DATABASE WHERE user={user}')
+    q = f'SELECT * FROM USER_DATABASE WHERE user={user}'
+    print(q)
+    nrecords_accessed = 1
+    return q, nrecords_accessed
 
 
 def admin_db_q():
-    print(f'SELECT * FROM ADMIN_DATABASE')
+    q = f'SELECT * FROM ADMIN_DATABASE'
+    print(q)
+    nrecords_accessed = 200
+    return q, nrecords_accessed
+
+
+@app.after_request
+def after(response):
+    # todo with response
+    # print('HTTP response')
+    # print(response.status)
+    # print(response.headers)
+    # print(response.get_data())
+    return response
+
+
+@app.before_request
+def before():
+    # todo with request
+    # e.g. print request.headers
+    # print(f'req headers: \n\t{request.headers}')
+    # print(f'req data: \n\t{request.get_data()}')
+    pass
 
 
 if __name__ == '__main__':
---
+++ clients.py
@@ -0,0 +1,71 @@
+# clients.py
+import requests
+import json
+import random
+import multiprocessing
+
+from app import NORMAL_USERS
+
+HOST = "localhost:8080"
+
+
+def run(i):
+    r = random.uniform(0, 1)
+    if r < 0.1:
+        run_admin()
+    else:
+        run_normal_user()
+
+def run_normal_user():
+    # flow: /login, /protected (anchor), /record
+    user = random.choice(NORMAL_USERS)
+    login_payload = {'username': user, 'password': 'test'}
+    access_token, status_code = run_login(login_payload)
+    run_protected(access_token)
+    user_record = get_user_record(access_token)
+    print(f'{user} successfully accessed his/her data {user_record}')
+
+def run_admin():
+    # flow: /login, /protected (anchor), /records
+    user = 'admin'
+    login_payload = {'username': user, 'password': 'test'}
+    access_token, status_code = run_login(login_payload)
+    run_protected(access_token)
+    admin_records = get_admin_records(access_token)
+    print(f'{user} successfully accessed all records: {admin_records}')
+
+def run_with_access_token(endpoint, access_token):
+    headers = {"Authorization": f'Bearer {access_token}'}
+    resp = requests.get(endpoint, headers=headers)
+    return json.loads(resp.content)
+
+def get_admin_records(access_token):
+    return run_with_access_token(f'http://{HOST}/records', access_token)
+
+def run_protected(access_token):
+    return run_with_access_token(f'http://{HOST}/protected', access_token)
+
+def run_clients(login_payload):
+    access_token, status_code = run_login(login_payload)
+    protected_resp = run_protected(access_token)
+    print(f'/protected resp: {protected_resp}')
+    user_record_resp = get_user_record(access_token)
+    print(f'/record resp: {user_record_resp}')
+
+def get_user_record(access_token):
+    return run_with_access_token(f'http://{HOST}/record', access_token)
+
+def run_login(login_payload):
+    resp = requests.post(f'http://{HOST}/login', json=login_payload)
+    try:
+        content = resp.content
+        json_data = json.loads(content)
+        access_code = json_data['access_token']
+        return access_code, resp.status_code
+    except:
+        return None, resp.status_code
+
+p = multiprocessing.Pool(processes=multiprocessing.cpu_count() - 1)
+p.map_async(run, range(100))
+p.close()
+p.join()
